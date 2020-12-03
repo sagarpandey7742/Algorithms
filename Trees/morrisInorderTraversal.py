815fab28b9fce514b3ffcc3dfcabc081eccab2e0
sagarpandey7742 <sagarpandey7742@gmail.com>
2020-12-03 18:55:52 +0530

    corrections to morrisInorderTraversal.py

--- Trees/morrisInorderTraversal.py
+++ Trees/morrisInorderTraversal.py
@@ -4,35 +4,43 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
+
 def morrisInorderTraversal(root):
-    current=root
+    current = root
     while current:
         if not current.left:
-            print(current.val,end=" ")
-            current=current.right
+            print(current.val, end=" ")
+            current = current.right
         else:
-            predecessor=current.left            # Finding the current's inorder predecessor
-            while predecessor.right and predecessor.right!=current:
-                predecessor=predecessor.right
+            predecessor = current.left  # Finding the current's inorder predecessor
+            while predecessor.right and predecessor.right != current:
+                predecessor = predecessor.right
 
-            if not predecessor.right:           # Making a link from predecessor->current
-                predecessor.right=current
-                current=current.left
-            else:                               # Restoring the tree be removing predecessor->current link
-                predecessor.right=None
-                print(current.val,end=" ")
-                current=current.right
+            if not predecessor.right:  # Making a link from predecessor->current
+                predecessor.right = current
+                current = current.left
+            else:  # Restoring the tree be removing predecessor->current link
+                predecessor.right = None
+                print(current.val, end=" ")
+                current = current.right
 
-root=TreeNode(13,None,None)
-root.left=TreeNode(8,None,None)
-root.right=TreeNode(18,None,None)
-r=root
-root=root.left
-root.left=TreeNode(6,None,None)
-root.right=TreeNode(11,None,None)
-root=r.right
-root.left=TreeNode(14,None,None)
-root.right=TreeNode(20,None,None)
-root=r
+
+root = TreeNode(13, None, None)
+root.left = TreeNode(8, None, None)
+root.right = TreeNode(18, None, None)
+r = root
+root = root.left
+root.left = TreeNode(6, None, None)
+root.right = TreeNode(11, None, None)
+root = r.right
+root.left = TreeNode(14, None, None)
+root.right = TreeNode(20, None, None)
+root = r
 morrisInorderTraversal(root)
 
+#       13
+#     /    \
+#    8      18
+#  /   \    /   \
+# 6    11   14   20
