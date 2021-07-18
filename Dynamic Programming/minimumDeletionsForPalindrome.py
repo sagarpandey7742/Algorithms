e1a44fbad75f4599f938a4947daa0c4395879a0b
sagarpandey7742 <sagarpandey7742@gmail.com>
2021-07-18 13:00:37 +0530

    minimumDeletionsForPalindrome.py

---
+++ Dynamic Programming/minimumDeletionsForPalindrome.py
@@ -0,0 +1,17 @@
+def fun(s):
+    t=s[::-1]
+    m=len(s)
+    n=len(t)
+    dp = [[0 for j in range(n + 1)] for i in range(m + 1)]
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if s[i - 1] == t[j - 1]:
+                dp[i][j] = 1 + dp[i - 1][j - 1]
+            else:
+                dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+
+    maxlength = dp[m][n]
+
+    return m-maxlength
+
+print(fun("aebcbda"))
