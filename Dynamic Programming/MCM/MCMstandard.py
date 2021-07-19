a2c3d0711810665eba454591d52a52ee2f67b976
sagarpandey7742 <sagarpandey7742@gmail.com>
2021-07-19 11:17:51 +0530

    MCMstandard.py

---
+++ Dynamic Programming/MCM/MCMstandard.py
@@ -0,0 +1,21 @@
+arr=[1, 2, 3, 4, 3]
+n=len(arr)
+dp=[[-1 for j in range(n)] for i in range(n)]
+
+def mcm(arr, i, j, ans):
+    if dp[i][j]!=-1:
+        return dp[i][j]
+    if i>=j:
+        dp[i][j]=0
+        return 0
+    for k in range(i, j):
+        temp= mcm(arr, i, k, ans) + mcm(arr, k+1, j, ans)+ arr[i-1]*arr[k]*arr[j]
+        ans=min(ans, temp)
+    dp[i][j]=ans
+    return dp[i][j]
+
+
+ans=10**10
+print(mcm(arr, 1, n-1, ans))
+
+
